0cfc7460c697546700a33e78935571ebc912e031
krsteva-odg <40174611+krsteva-odg@users.noreply.github.com>
2021-06-15 17:44:41 -0500

    Update moc_monitors.py

--- moc_monitors.py
+++ moc_monitors.py
@@ -31,7 +31,6 @@ class DriftDetector:
     """
     A class to compute differences (by some metric) between two DataFrames:
     a baseline and a sample.
-    AN ADDITIONAL CHANGE HERE TO CAUSE VERSION UPDATE
 
     Methods
     -------
