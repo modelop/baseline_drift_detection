0a8779e1229bd37f9980aea5933b4076582acfaf
krsteva-odg <blagica.krsteva@modelop.com>
2021-06-21 15:44:12 -0500

    NEW CHANGE

--- monitor.py
+++ monitor.py
@@ -9,6 +9,7 @@ from moc_schema_infer import set_detector_parameters
 
 # modelop.init
 def begin():
+    # A comment to trigger a change
     print("Begin function...")
     global SPARK
     SPARK = SparkSession.builder.appName("DriftTest").getOrCreate()
