f0f04eef77554017d888c217703132abb9e38986
krsteva-odg <blagica.krsteva@modelop.com>
2021-06-23 15:41:10 -0500

    a new comment

--- monitor.py
+++ monitor.py
@@ -10,6 +10,7 @@ from moc_schema_infer import set_detector_parameters
 # modelop.init
 def begin():
     # adding a comment to cause a change
+    # a new comment
     print("Begin function...")
     global SPARK
     SPARK = SparkSession.builder.appName("DriftTest").getOrCreate()
